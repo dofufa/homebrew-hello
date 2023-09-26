66fbd7ca871200e2254e6b15bf839fce3605a04e
Dofufa Labs <121826629+dofufa@users.noreply.github.com>
2023-09-26 11:13:34 -0700

    Update hello.rb

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -2,7 +2,7 @@ class Hello < Formula
   desc "Test repo: hello world."
   homepage "https://test.dofufa.com/"
   url "https://github.com/dofufa/homebrew-hello/archive/v2.tar.gz"
-  sha256 "a0bfba64f97bfbeee4f62bf06c22bad45872a3dc44a0e0486a9e087584cc9dec"
+  sha256 "ee45e8766c66bfe21f4cb515e49c5e22ad255b42452c5a9010a5b7918d41ba74"
   head "https://github.com/dofufa/homebrew-hello.git", branch: "main"
   license "MIT"
   revision 1
