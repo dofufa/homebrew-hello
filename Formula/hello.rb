4881a9ee3b39de1509d53c5ee729affc0bd556f4
Dofufa Labs <121826629+dofufa@users.noreply.github.com>
2023-09-26 07:05:09 -0700

    Create hello.rb

---
+++ Formula/hello.rb
@@ -0,0 +1,18 @@
+class Hello < Formula
+  desc "Test repo: hello world."
+  homepage "https://test.dofufa.com/"
+  url "https://github.com/dofufa/homebrew-hello/archive/v1.tar.gz"
+  sha256 ""
+  head "https://github.com/dofufa/homebrew-hello.git"
+  license "MIT"
+  revision 1
+
+  def install
+    ohai "DOFUFA: *drumroll* ... installing dofufa-hello ";
+    bin.install "bin/hello.php" => "dofufa-hello"
+  end
+
+  test do
+    system bin/"dofufa-hello", "--version"
+  end
+end
